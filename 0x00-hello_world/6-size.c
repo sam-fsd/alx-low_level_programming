621fff8523aca2f385e7f2e022c39d0d491ce261
sam-fsd <kabirusam037@gmail.com>
2023-03-16 08:46:23 -0700

    truncate chars to 80 chars per line

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,17 +5,12 @@
  */
 int main(void)
 {
-	int d;
-	char c;
-	long int Lnum;
-	long long int LlongI;
-	float fl;
+	int d, char c, long int Lnum, long long int lli, float fl;
 
-	printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(Lnum));
-	printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(LlongI));
-	printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(fl));
-
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(Lnum));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
+	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(fl));
 	return (0);
 }
