4a6b9d97b96fcdf9e37caaed2cffae4cfd183d50
sam-fsd <kabirusam037@gmail.com>
2023-03-17 05:42:44 -0700

    Fix: modified expression of if

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,11 +17,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	last_num = n % 10;
 
-	if (n > 5)
+	if (last_num > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, last_num);
 	}
-	else if (n < 6 && n == 0)
+	else if (last_num < 6 && last_num == 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_num);
 	}
