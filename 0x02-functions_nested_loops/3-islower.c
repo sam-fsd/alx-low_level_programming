d1486f7473f2d0c99899ac5c0da3c1661cdb97fa
sam-fsd <kabirusam037@gmail.com>
2023-03-21 09:16:25 -0700

    modified code to only have function definition

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,14 @@
 #include "main.h"
 
-int _islower(int c);
 /**
- * main - invokes _islower
- * Return: 0 success
+ * _islower - checks if a character is lowercase
+ * Return: 0 if its not lowercase and 1 if it is
+ *@c: character to be checked
  */
-int main(void)
+int _islower(int c)
 {
-	int r, j;
-
-	r = _islower('H');
-	j = _islower('h');
-
-	putchar(r + '0');
-	putchar(j + '0');
-	putchar('\n');
-
-	return (0);
+	if (islower(c) == 0)
+		return (0);
+	else
+		return (1);
 }
