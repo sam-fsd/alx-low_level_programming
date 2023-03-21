eba904d2a11dfd714e4593cf3546aab0f2fb34a3
sam-fsd <kabirusam037@gmail.com>
2023-03-21 09:30:44 -0700

    renamed file

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - checks if a number is +ve, -ve or 0 and prints their signs
+ * Return: 1 if +ve -1 if -ve 0 if its 0
+ * @n: number to be checked
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		printf("+");
+		return (1);
+	}
+	else if (n < 0)
+	{
+		printf("-");
+		return (-1);
+	}
+	else
+	{
+		printf("0");
+		return (0);
+	}
+}
