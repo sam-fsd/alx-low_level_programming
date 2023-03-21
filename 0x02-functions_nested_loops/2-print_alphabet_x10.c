8ecaed99428eebec49d7e9f943217e72df0ffa90
sam-fsd <kabirusam037@gmail.com>
2023-03-21 09:09:33 -0700

    function definition added

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,19 @@
 #include "main.h"
 
-void print_alphabet_x10(void);
-
 /**
- * main - invokes print_alphabet()
- * Return: 0 success
+ * print_alphabet_x10 - prints the alphabets 10 times
  */
-int main(void)
+void print_alphabet_x10(void)
 {
-	print_alphabet_x10();
-	return (0);
+	int i;
+	char c;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			putchar(c);
+		}
+		putchar('\n');
+	}
 }
