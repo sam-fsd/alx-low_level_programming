059298cbed4c2d51fae896d5606d2efb07b12ac3
sam-fsd <kabirusam037@gmail.com>
2023-03-21 07:23:25 -0700

    removed function-prototypes declaration

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,15 +2,6 @@
 #include <ctype.h>
 #include <stdlib.h>
 
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int _abs(int n);
-int print_sign(int n);
-int print_last_digit(int);
-
-
 
 void print_alphabet(void)
 {
