859799b618d49b87ed0d8d06dade564261f76b3c
sam-fsd <kabirusam037@gmail.com>
2023-03-21 09:24:55 -0700

    modified code to only have function definition

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,20 +1,15 @@
 #include "main.h"
+#include <ctype.h>
 
-int _isalpha(int c);
 /**
- * main - invokes _islower()
- * Return: 0 success
+ * _isalpha - check if a character is alphabetic character
+ * Return: 0 if its not an alphabetic character 1 if it is
+ * @c: character to be checked
  */
-int main(void)
+int _isalpha(int c)
 {
-	int a, b, c;
-
-	a = _isalpha('a');
-	b = _isalpha('+');
-
-	putchar(a + '0');
-	putchar(b + '0');
-	putchar('\n');
-
-	return (0);
+	if (isalpha(c) == 0)
+		return (0);
+	else
+		return (1);
 }
