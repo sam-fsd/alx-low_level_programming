d88d0fcc482a7b0ea262bc1d9dd267ecaf1af6cd
sam-fsd <kabirusam037@gmail.com>
2023-03-23 06:11:50 -0700

    adde betty-style documentation

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include "ctype.h"
 
+/**
+ * _isupper - Checks if a char is uppercase
+ * @n: character to be checked
+ * Return: 1 if uppercase 0 if not
+ */
 int _isupper(int n)
 {
 	if (n >= 65 && n <= 90)
