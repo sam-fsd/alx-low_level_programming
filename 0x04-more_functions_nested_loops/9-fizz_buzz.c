0b1e633d9aaa77cf961f4349beb00dbe18213298
sam-fsd <kabirusam037@gmail.com>
2023-03-23 10:22:22 -0700

    modified code to be smaller

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,26 +1,13 @@
 #include <stdio.h>
 
-void fizzbuzz(void);
-
 /**
- * main - invokes fizzbuzz()
- * Return: 0 (success)
- */
-int main(void)
-{
-	fizzbuzz();
-	putchar('\n');
-
-	return (0);
-}
-
-/**
- * fizzbuzz - prints 0 to 100
+ * main - prints 0 to 100
  * for mutliples of 3 it prints Fizz
  * for multiples of 5 it prints Buzz
  * and for both multiples it prints FizzBuzz
+ * Return: 0 (success)
  */
-void fizzbuzz(void)
+int main(void)
 {
 	int i = 1;
 
@@ -32,19 +19,23 @@ void fizzbuzz(void)
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Buzz");
+			printf("Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Fizz");
+			printf("Buzz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%i", i);
 		}
 		if (i != 100)
-			putchar('\n');
+		{
+			putchar(' ');
+		}
 
 		i++;
 	}
+	putchar('\n');
+	return (0);
 }
