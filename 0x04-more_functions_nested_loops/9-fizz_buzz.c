c708997715aa33e5e027e962e45331493d7ecf88
sam-fsd <kabirusam037@gmail.com>
2023-03-23 10:13:26 -0700

    added `if` to add spaceing dynamically

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,10 +8,10 @@ void fizzbuzz(void);
  */
 int main(void)
 {
-        fizzbuzz();
-        putchar('\n');
+	fizzbuzz();
+	putchar('\n');
 
-        return (0);
+	return (0);
 }
 
 /**
@@ -28,20 +28,23 @@ void fizzbuzz(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else
 		{
 			printf("%d ", i);
 		}
+		if (i != 100)
+			putchar('\n');
+
 		i++;
 	}
 }
