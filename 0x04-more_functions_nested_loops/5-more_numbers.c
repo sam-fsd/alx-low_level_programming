bf5e55146e43e66d84d326bcd608d771089d0df8
sam-fsd <kabirusam037@gmail.com>
2023-03-23 07:40:21 -0700

    prints 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,9 +7,22 @@
 void more_numbers(void)
 {
 	int i;
+	char a, b;
 
-	for (i = 0; i <= 14; i++)
-		_putchar(i + '0');
+	for (i = 0; i < 10; i++)
+	{
+		for (a = 0; a <= 14; a++)
+		{
+			b = a;
 
-	_putchar('\n');
+			if (a > 9)
+			{
+				_putchar('1');
+				b = a % 10;
+			}
+			_putchar('0' + b);
+		}
+		_putchar('\n');
+	}
+
 }
