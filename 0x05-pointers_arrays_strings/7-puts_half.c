c417bcaeb03a8b98b7453e9ddab5fc7cbc949f8d
sam-fsd <kabirusam037@gmail.com>
2023-03-27 10:44:48 -0700

    added check incase size of array is odd

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,6 +11,9 @@ void puts_half(char *str)
 	int mid = len / 2;
 	int i;
 
+	if ((len % 2) == 1)
+		mid = ((len + 1) / 2);
+
 	for (i = mid; str[i] != '\0'; i++)
 		_putchar(str[i]);
 	_putchar('\n');
