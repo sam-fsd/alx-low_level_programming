3fe97700046749d5ab3e4216ee04ba29d7b9aaa6
sam-fsd <kabirusam037@gmail.com>
2023-03-27 10:55:57 -0700

    Refactored code to address big array input

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,12 +10,13 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < (n - 1); i++)
 	{
-		if (i != (n - 1))
-			printf("%d, ", a[i]);
-		else
-			printf("%d", a[i - 1]);
+		printf("%d, ", a[i]);
 	}
-	_putchar('\n');
+		if (i == (n - 1))
+		{
+			printf("%d", a[n - 1]);
+		}
+			printf("\n");
 }
