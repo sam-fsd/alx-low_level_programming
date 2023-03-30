6d2a13baeedf53a01bcea97a13587326a7b693bb
sam-fsd <kabirusam037@gmail.com>
2023-03-30 08:09:06 -0700

    added function prototypes

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,5 +8,8 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *cap_string(char *);
 char *leet(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
 #endif
