20015418b8f12e815b456869a653c679ce2ce64a
sam-fsd <kabirusam037@gmail.com>
2023-03-30 08:36:52 -0700

    update

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+ * main - prints character
+ * Return: alaways 0
+ */
 int main(void)
 {
 	int n;
@@ -8,7 +12,7 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-	/*
+	/**
 	* write your line of code here...
 	* Remember:
 	* - you are not allowed to use a
