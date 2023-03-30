2cd59bd7ea30d0e2937de5c087def874536eb8c3
sam-fsd <kabirusam037@gmail.com>
2023-03-30 00:43:15 -0700

    added betty documentation style

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * strncat - appends n bytes of src to dest
+ * _strncat - appends n bytes of src to dest
  * @dest: array accepting n elemnts of src
  * @src: n elements to be appendend to dest
+ * @n: number of bytes
  * Return: a pointer to dest
  */
 char *_strncat(char *dest, char *src, int n)
