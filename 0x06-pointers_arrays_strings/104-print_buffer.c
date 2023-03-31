0d8b3d35cea8faa108e2f19c173045d6e0e030df
Sam Kabiru <61576146+sam-fsd@users.noreply.github.com>
2023-03-31 19:36:08 +0300

    Updates

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,37 +2,37 @@
 #include <stdio.h>
 
 /**
- * print_buffer - Prints a buffer 10 bytes at a time, starting with
- *                the byte position, then showing the hex content,
- *                then displaying printable charcaters.
- * @b: The buffer to be printed.
- * @size: The number of bytes to be printed from the buffer.
+ * print_buffer - prints a buffer 10 bytes at a time beginning with
+ * the byte position,shows the hex content,
+ * and finally displaying printable charcaters.
+ * @b: buffer to be printed.
+ * @size: number of bytes to be printed from the buffer.
  */
 
 void print_buffer(char *b, int size)
 {
-	int byte, index;
+	int byte, i;
 
 	for (byte = 0; byte < size; byte += 10)
 	{
 		printf("%08x: ", byte);
 
-		for (index = 0; index < 10; index++)
+		for (i = 0; i < 10; i++)
 		{
-			if ((index + byte) >= size)
+			if ((i + byte) >= size)
 				printf("  ");
 			else
-				printf("%02x", *(b + index + byte));
-			if ((index % 2) != 0 && index != 0)
+				printf("%02x", *(b + i + byte));
+			if ((i % 2) != 0 && i != 0)
 				printf(" ");
 		}
-		for (index = 0; index < 10; index++)
+		for (i = 0; i < 10; i++)
 		{
-			if ((index + byte) >= size)
+			if ((i + byte) >= size)
 				break;
-			else if (*(b + index + byte) >= 31 &&
-				 *(b + index + byte) <= 126)
-				printf("%c", *(b + index + byte));
+			else if (*(b + i + byte) >= 31 &&
+				 *(b + i + byte) <= 126)
+				printf("%c", *(b + i + byte));
 			else
 				printf(".");
 		}
