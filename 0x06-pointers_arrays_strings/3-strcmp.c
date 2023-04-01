e90a17a45010de96a08f95a8ffe457003046348e
Sam Kabiru <61576146+sam-fsd@users.noreply.github.com>
2023-04-01 12:01:09 +0300

    Updates

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,12 +4,10 @@
  * _strcmp - Compares pointers to two strings.
  * @s1: A pointer to the first string to be compared.
  * @s2: A pointer to the second string to be compared.
- *
- * Return: If str1 < str2, the negative difference of
- * the first unmatched characters.
- *         If s1 == s2, 0.
- *         If s1 > s2, the positive difference of
- *         the first unmatched characters.
+ * Return: the negative difference of
+ * the first unmatched characters If s1 < s2, 
+ * If s1 > s2 return a positive difference of the first
+ * chars that don't match
  */
 int _strcmp(char *s1, char *s2)
 {
