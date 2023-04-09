9115d3d2571ab79ca75506b6eb12743d7ef38399
sam-fsd <kabirusam037@gmail.com>
2023-04-09 10:18:07 -0700

    Used stdlib to return success at runtime

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,14 +2,18 @@
 
 /**
  * main - prints the number of arguments
- * @argc: numner of arguments
+ * @argc: number of arguments
  * @argv: arguments passed
  * Return: 0(success)
  */
-int main(int argc, char *argv)
+int main(int argc, char **argv)
 {
+	int args;
+
 	if (argc > 1)
 		printf("%d\n", argc - 1);
 	else
 		printf("0\n");
+
+	exit(EXIT_SUCCESS);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,5 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #endif
