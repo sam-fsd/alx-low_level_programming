e1191c95246194787c6bc4ea095fd27617043034
sam-fsd <kabirusam037@gmail.com>
2023-04-09 08:42:24 -0700

    prints the name of the file with its path

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file
+ * @argc: number of arguments passed to argv
+ * @argv: array of strings(arguments)
+ * Return: O(success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
