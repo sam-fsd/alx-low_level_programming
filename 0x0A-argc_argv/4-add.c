db0cdf13d597502bd1ffd9469a2b77820f45277b
sam-fsd <kabirusam037@gmail.com>
2023-04-10 06:20:37 -0700

    Code refactor

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,27 +9,32 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, j, length, sum;
+	char *ptr;
 
-	if (argc == 1 || argc == 2)
+	if (argc < 2)
+	printf("0\n");
+	else
 	{
-		printf("0\n");
-		return (0);
-	}
-	
-	for (i = 1; i < argc; i++)
-	{
-		if (!isdigit(*argv[i]) || *argv[i] < 0)
+		sum = 0;
+		for (i = 1; i < argc; i++)
 		{
-			printf("Error\n");
-			return (1);
+			ptr = argv[i];
+			length = strlen(ptr);
+
+			for (j = 0; j < length; j++)
+			{
+				if (isdigit(*(ptr + j)) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+
+			sum += atoi(argv[i]);
 		}
+
+		printf("%d\n", sum);
 	}
-
-	for (i = 1; i < argc; i++)
-		sum += atoi(argv[i]);
-
-	printf("%d\n", sum);
-
 	return (0);
 }
