b638539fa05562b2c70cc9ac3df0b1c3234019fd
sam-fsd <kabirusam037@gmail.com>
2023-04-12 15:21:16 -0700

    Fix: betty coding style

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -4,13 +4,13 @@
  * free_grid - frees memory held by a 2D array of integer
  * pointer variable
  * @grid: double pointer to the array
- * @height; the size of the array
+ * @height: the size of the array
  */
 void free_grid(int **grid, int height)
 {
 	int i;
 
-	for ( i = 0; i < height; i++)
+	for (i = 0; i < height; i++)
 		free(grid[i]);
 
 	free(grid);
