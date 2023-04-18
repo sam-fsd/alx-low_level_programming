4e0adabc9b205475677919a7f0901bc8f4074d88
sam-fsd <kabirusam037@gmail.com>
2023-04-18 03:14:57 -0700

    a function that intilizes a variable of type struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * init_dog - initilizes a variable of type struct dog
+ * @d: pointer to struct
+ * @name: pointer value
+ * @age: pointer value
+ * @owner: pointer to char
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
