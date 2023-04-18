f3a97e1ed000f2d2d6c5b5d3ac41f41198c02fc8
sam-fsd <kabirusam037@gmail.com>
2023-04-18 10:42:02 -0700

    dynamic allocated space for members of type char *

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,7 @@
 #include "dog.h"
 
 /**
- * new_dog -creates a new dog
+ * new_dog - creates a new dog
  * @name: pointer to a string
  * @age: age to be intilized
  * @owner: pointer to a string
@@ -11,10 +11,28 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ptr;
 
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
+
 	ptr = malloc(sizeof(dog_t));
 
 	if (ptr == NULL)
 		return (NULL);
+
+	ptr->name = malloc(strlen(name) + 1);
+	if (ptr->name == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	ptr->owner = malloc(strlen(owner) + 1);
+	if (ptr->owner == NULL)
+	{
+		free(ptr->name);
+		free(ptr);
+		return (NULL);
+	}
 
 	ptr->name = strdup(name);
 	ptr->age = age;
