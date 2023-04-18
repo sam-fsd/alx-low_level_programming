0a1ab1c91d1dbfc23ed3d525dedeb990b7dee59e
sam-fsd <kabirusam037@gmail.com>
2023-04-18 10:48:25 -0700

    a function that frees memory of struct dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * free_dog - frees dog structure if it exits
+ * @d: pointer to dog
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return (NULL);
+
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
