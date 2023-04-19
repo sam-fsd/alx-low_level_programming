47e9740063639734bbac920bfe5f54ee16dd20f0
sam-fsd <kabirusam037@gmail.com>
2023-04-19 11:13:59 -0700

    documentated header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,13 @@
 #ifndef FUNCTION_POINTERS
 #define FUNCTION_POINTERS
 
+/*
+ * Desc: Header file containing prototypes for all functions
+ * used in this directory.
+ */
+
+#include <stdlib.h>
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
