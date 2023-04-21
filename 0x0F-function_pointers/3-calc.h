34fafd514867bc67dd8df9e396d0f6c62c330d83
sam-fsd <kabirusam037@gmail.com>
2023-04-21 11:07:03 -0700

    added a comment description

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,6 +1,11 @@
 #ifndef CALC_H
 #define CALC_H
 
+/*
+ * Desc: header file containing all structures and
+ * prototypes used by the 3-main.c program.
+ */
+
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
@@ -16,8 +21,8 @@ int op_mod(int a, int b);
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
 #endif
