701f86e3765bfc317177c3474bbad8b102c27b13
sam-fsd <kabirusam037@gmail.com>
2023-05-01 09:27:14 -0700

    new struct implementation

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,20 +2,19 @@
 #define LISTS_H
 
 /**
- * struct node - a node that has an int data
- * @n: integer element
- * @next: pointer to a node
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
  *
- * Description: It represents an element/node in a linked list
+ * Description: singly linked list node structure
+ * 
  */
-typedef struct node
+typedef struct listint_s
 {
-	int n;
-	struct node *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
 
-size_t print_listint(const listint_t *h);
-size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
