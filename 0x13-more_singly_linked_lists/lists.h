5ead49bfe5941995e5b8c820010917c14b0d5600
sam-fsd <kabirusam037@gmail.com>
2023-05-01 09:23:08 -0700

    header file containing function declarations and structure node

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,29 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/**
+ * struct node - a node that has an int data
+ * @n: integer element
+ * @next: pointer to a node
+ *
+ * Description: It represents an element/node in a linked list
+ */
+typedef struct node
+{
+	int n;
+	struct node *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+void free_listint2(listint_t **head);
+int pop_listint(listint_t **head);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+int sum_listint(listint_t *head);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+
+#endif
