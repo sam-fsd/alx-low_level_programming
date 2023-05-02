4d036504627cd343e7bdc30299444b2a42b9d21e
sam-fsd <kabirusam037@gmail.com>
2023-05-02 10:21:23 -0700

    Handles edge-case where idx might be 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,6 +26,15 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	current = *head;
 	prev = NULL;
+
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+
+		return (new);
+	}
+
 	for (i = 0; i < idx; i++)
 	{
 		if (current->next == NULL)
@@ -35,11 +44,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		current = current->next;
 	}
 	new->next = current;
-
-	if (prev != NULL)
-		prev->next = new;
-	else
-		*head = new;
+	prev->next = new;
 
 	return (new);
 }
