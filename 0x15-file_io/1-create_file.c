1e4d9c31360df941b1cbe832db829c779853139f
sam-fsd <kabirusam037@gmail.com>
2023-05-08 14:55:12 -0700

    a function that creates a file.

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * create_file - creates a file if does not exist and writes
+ * text to it
+ * @filename: file to be created
+ * @text_content: pointer to a string
+ *
+ * Return: 1 if success -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t fd, fw;
+	int len;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	len = strlen(text_content);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fw = write(fd, text_content, len);
+
+	if (fd == -1 || fw == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
