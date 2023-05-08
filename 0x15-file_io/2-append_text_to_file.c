aca2b95a6ebf690da5ffb76ad0fdda2576290f88
sam-fsd <kabirusam037@gmail.com>
2023-05-08 15:48:28 -0700

    got string length with a loop

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,13 +10,16 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, fw, len;
+	int fd, fw, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
-		len = strlen(text_content);
+	{
+		for(len = 0; text_content[len];)
+			len++;
+	}
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 	fw = write(fd, text_content, len);
