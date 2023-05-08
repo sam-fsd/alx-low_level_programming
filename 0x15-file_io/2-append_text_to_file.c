3efc2709f5a91bc60d48749aaea3ab3b6d3369bc
sam-fsd <kabirusam037@gmail.com>
2023-05-08 16:07:47 -0700

    betty guideline

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,9 +17,8 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for(len = 0; text_content[len];)
+		for (len = 0; text_content[len];)
 			len++;
-	}
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 	fw = write(fd, text_content, len);
